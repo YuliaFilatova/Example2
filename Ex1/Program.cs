4d2ca273568a2db8bdf00888030a0a16ecc34e2f
Yuliya <filatovajulia@rambler.ru>
2022-09-05 21:58:53 +0300

    add Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -3,4 +3,4 @@
 Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+Console.WriteLine("вторая цифра этого числа - "+stringNumber[1]);
